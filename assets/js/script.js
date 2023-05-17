7ecba8b701cba8c1d69f8f9577030623c2f715bd
Felipe <lfazocar@users.noreply.github.com>
2023-05-17 13:44:00 -0400

    Scrolling en CSS en vez de JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,5 @@
 $(document).ready(function(){
 
-    // Smooth scrolling
-    $(".nav-link").on('click', function(event){
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top-100
-            }, 0);
-        }
-    });
-
     // Habilitar tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
